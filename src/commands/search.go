48d38a61c8e906723203a688b433b67e4fc8b092
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-08-20 18:16:06 -0300

    :sparkles: feat: add Search command and update to 1.2.0

---
+++ src/commands/search.go
@@ -0,0 +1,43 @@
+/*
+Copyright © 2023 Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
+*/
+package commands
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/jedib0t/go-pretty/v6/text"
+	"github.com/kauefraga/anime-archive/src/infra"
+	"github.com/kauefraga/anime-archive/src/ui"
+	"github.com/spf13/cobra"
+)
+
+func Search() *cobra.Command {
+	search := &cobra.Command{
+		Use:     "search [TITLE]",
+		Short:   "Search for an anime with a title and return it with a url",
+		Example: "anime-archive search 'Mushoku Tensei'",
+		Args:    cobra.ExactArgs(1),
+		Run: func(command *cobra.Command, arguments []string) {
+			if strings.TrimSpace(arguments[0]) == "" {
+				fmt.Println(ui.Minus, text.FgRed.Sprint("You need to write a title."))
+				os.Exit(1)
+			}
+
+			fmt.Println(ui.Interrogative, "Searching...")
+
+			database := infra.InitDatabase()
+
+			var anime infra.Anime
+			database.Where("title LIKE ?", "%"+arguments[0]+"%").First(&anime)
+
+			fmt.Println(text.FgGreen.Sprint("Found!"))
+			fmt.Println(ui.Plus, "Title:", anime.Title)
+			fmt.Println(ui.Plus, "Url:", anime.Url)
+		},
+	}
+
+	return search
+}
--- src/main.go
+++ src/main.go
@@ -14,12 +14,13 @@ func main() {
 	rootCmd := &cobra.Command{
 		Use:     "anime-archive [command] [arguments] [options]",
 		Short:   "🦋 A command line interface to create, find and list all my viewed animes.",
-		Version: "1.1.1",
+		Version: "1.2.0",
 	}
 
 	rootCmd.AddCommand(commands.Status())
 	rootCmd.AddCommand(commands.List())
 	rootCmd.AddCommand(commands.Store())
+	rootCmd.AddCommand(commands.Search())
 
 	err := rootCmd.Execute()
 
