70615d479584e005c459c4847de35ac1d1c7db1b
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-11-04 23:25:43 -0300

    :sparkles: feat: implement head and tail flags

--- src/commands/list.go
+++ src/commands/list.go
@@ -15,11 +15,19 @@ import (
 )
 
 func List() *cobra.Command {
+	var tail uint
+	var head uint
+
 	list := &cobra.Command{
 		Use:     "list",
 		Short:   "Query and show all the registered animes",
 		Example: "anime-archive list",
 		Run: func(command *cobra.Command, arguments []string) {
+			if tail > 0 && head > 0 {
+				fmt.Println(ui.Minus, "Choose tail or head.")
+				os.Exit(1)
+			}
+
 			database := infra.ConnectDatabase()
 
 			fmt.Println(ui.Interrogative, "Querying animes...")
@@ -29,9 +37,20 @@ func List() *cobra.Command {
 			fmt.Println(ui.Plus, "Animes count:", amountOfAnimes)
 
 			var animes []infra.Anime
-			database.Select(
-				"title", "created_at",
-			).Order("created_at DESC").Find(&animes)
+
+			if head > 0 {
+				database.Select(
+					"title", "created_at",
+				).Order("id DESC").Limit(int(head)).Find(&animes)
+			} else if tail > 0 {
+				database.Select(
+					"title", "created_at",
+				).Limit(int(tail)).Find(&animes)
+			} else {
+				database.Select(
+					"title", "created_at",
+				).Order("created_at DESC").Find(&animes)
+			}
 
 			t := table.NewWriter()
 			t.SetOutputMirror(os.Stdout)
@@ -52,6 +71,9 @@ func List() *cobra.Command {
 			t.Render()
 		},
 	}
+
+	list.Flags().UintVarP(&tail, "tail", "T", 0, "Query the oldest records")
+	list.Flags().UintVarP(&head, "head", "H", 0, "Query the newest records")
 
 	return list
 }
