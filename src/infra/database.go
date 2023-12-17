3ca65e5ce1fd6c200f602879466b0661cee127b3
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-12-17 17:18:48 -0300

    :recycle: refactor: remove duplicate code

--- src/infra/database.go
+++ src/infra/database.go
@@ -4,7 +4,6 @@ Copyright © 2023 Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
 package infra
 
 import (
-	"os"
 	"time"
 
 	"github.com/adrg/xdg"
@@ -40,27 +39,16 @@ func InitDatabase() (*gorm.DB, string) {
 	return database, databasePath
 }
 
-func SearchDatabase() string {
-	databasePath, err := xdg.SearchDataFile(".anime-archive/animes.db")
-
-	if err != nil {
-		panic(err)
-	}
-
-	return databasePath
-}
-
-func HasDatabaseAlready() bool {
-	// Duplicate code (func SearchDatabase)
-	databasePath, _ := xdg.SearchDataFile(".anime-archive/animes.db")
-
-	_, err := os.Stat(databasePath)
-
-	return err == nil
+func SearchDatabase() (string, error) {
+	return xdg.SearchDataFile(".anime-archive/animes.db")
 }
 
 func ConnectDatabase() *gorm.DB {
-	databasePath := SearchDatabase()
+	databasePath, err := SearchDatabase()
+
+	if err != nil {
+		panic("Failed to find the database file.")
+	}
 
 	database, err := gorm.Open(
 		sqlite.Open(databasePath),
