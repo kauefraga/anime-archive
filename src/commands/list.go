e10f422b2ce6a60fbf057b642e593ce8719469d9
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-12-21 10:57:13 -0300

    :recycle: refactor: create and fill anime table in ui layer

--- src/commands/list.go
+++ src/commands/list.go
@@ -7,8 +7,6 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/jedib0t/go-pretty/v6/table"
-	"github.com/jedib0t/go-pretty/v6/text"
 	"github.com/kauefraga/anime-archive/src/infra"
 	"github.com/kauefraga/anime-archive/src/ui"
 	"github.com/spf13/cobra"
@@ -52,23 +50,7 @@ func List() *cobra.Command {
 				).Order("created_at DESC").Find(&animes)
 			}
 
-			t := table.NewWriter()
-			t.SetOutputMirror(os.Stdout)
-			t.AppendHeader(table.Row{
-				text.FgMagenta.Sprint("Title"),
-				text.FgCyan.Sprint("Created at"),
-			})
-
-			for _, anime := range animes {
-				year, month, day := anime.CreatedAt.UTC().Date()
-
-				t.AppendRow(table.Row{
-					anime.Title,
-					fmt.Sprint(day, " ", month, " ", year),
-				})
-			}
-
-			t.Render()
+			ui.ShowAnimesTable(animes)
 		},
 	}
 
---
+++ src/ui/animes-table.go
@@ -0,0 +1,33 @@
+/*
+Copyright © 2023 Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
+*/
+package ui
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
+	"github.com/kauefraga/anime-archive/src/infra"
+)
+
+func ShowAnimesTable(animes []infra.Anime) {
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{
+		text.FgMagenta.Sprint("Title"),
+		text.FgCyan.Sprint("Created at"),
+	})
+
+	for _, anime := range animes {
+		year, month, day := anime.CreatedAt.UTC().Date()
+
+		t.AppendRow(table.Row{
+			anime.Title,
+			fmt.Sprint(day, " ", month, " ", year),
+		})
+	}
+
+	t.Render()
+}
