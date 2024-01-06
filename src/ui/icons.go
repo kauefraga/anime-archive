691493a15b884d61d78345737a0e71dde30a2678
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2024-01-05 23:05:54 -0300

    :recycle: refactor: declare icons with one var

--- src/ui/icons.go
+++ src/ui/icons.go
@@ -2,6 +2,8 @@ package ui
 
 import "github.com/jedib0t/go-pretty/v6/text"
 
-var Plus = text.FgGreen.Sprint("[") + "+" + text.FgGreen.Sprint("]")
-var Minus = text.FgRed.Sprint("[") + "-" + text.FgRed.Sprint("]")
-var Interrogative = text.FgYellow.Sprint("[") + "?" + text.FgYellow.Sprint("]")
+var (
+	Plus          = text.FgGreen.Sprint("[") + "+" + text.FgGreen.Sprint("]")
+	Minus         = text.FgRed.Sprint("[") + "-" + text.FgRed.Sprint("]")
+	Interrogative = text.FgYellow.Sprint("[") + "?" + text.FgYellow.Sprint("]")
+)
