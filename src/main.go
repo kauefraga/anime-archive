1f845c4bcefba436b5a1c81a1920a24f2e9faabf
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2024-01-17 19:49:04 -0300

    :label: version: update to 1.7.0 and add update feature

--- src/main.go
+++ src/main.go
@@ -15,13 +15,14 @@ func main() {
 		Use:     "anime-archive",
 		Short:   "🦋 A command line interface to create, find and list all my viewed animes.",
 		Long:    "🦋 A command line interface to create, find and list all my viewed animes.\nCopyright © 2023 Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>",
-		Version: "1.6.0",
+		Version: "1.7.0",
 	}
 
 	rootCmd.AddCommand(commands.Setup())
 	rootCmd.AddCommand(commands.Status())
 	rootCmd.AddCommand(commands.List())
 	rootCmd.AddCommand(commands.Register())
+	rootCmd.AddCommand(commands.Update())
 	rootCmd.AddCommand(commands.Search())
 	rootCmd.AddCommand(commands.Export())
 
